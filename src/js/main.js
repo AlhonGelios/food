78bd0fb270b1ef49e612e844748b93afdb3f9fc1
Gelios <Geliosal39hon@gmail.com>
2020-08-01 16:05:02 +0300

    timer

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+    //Tabs
     const   tabs = document.querySelectorAll(".tabheader__item"),
             tabsContent = document.querySelectorAll(".tabcontent"),
             tabsParent = document.querySelector(".tabheader__items");
@@ -34,4 +35,64 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    //Timer
+
+    const deadline = '2020-08-02T16:00';
+
+    function getTimeRemaining(endtime) {
+        const   t = Date.parse(endtime) - Date.parse(new Date()),
+                days = Math.floor(t / (1000*60*60*24)),
+                hours = Math.floor((t / (1000*60*60)) % 24),
+                minutes = Math.floor((t / (1000*60)) % 60),
+                seconds = Math.floor((t / 1000) % 60);
+        
+        return {
+            'total' : t,
+            'days' : days,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const   timer = document.querySelector(selector),
+                days = timer.querySelector('#days'),
+                hours = timer.querySelector('#hours'),
+                minutes = timer.querySelector('#minutes'),
+                seconds = timer.querySelector('#seconds'),
+                endtimeText = document.querySelector('#endtime'),
+                timeInterval = setInterval(updateClock, 1000),
+                deadlineDate = new Date(endtime),
+                months = ['января' , 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября','ноября', 'декабря'];
+        
+        updateClock();
+
+        endtimeText.textContent = `Акция закончится ${getZero(deadlineDate.getDate())} ${months[deadlineDate.getMonth()]} в
+         ${getZero(deadlineDate.getHours())}:${getZero(deadlineDate.getMinutes())}`;
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
 });
