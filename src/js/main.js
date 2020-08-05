f5e61d71184ec9bb91f4a46809fa194923e4b79b
AlhonGelios <Geliosal39hon@gmail.com>
2020-08-05 14:46:52 +0300

    server

--- src/js/main.js
+++ src/js/main.js
@@ -231,4 +231,56 @@ window.addEventListener('DOMContentLoaded', () => {
         '.menu .container',
         'menu__item'
     ).render();
+
+    // forms
+
+    const forms= document.querySelectorAll('form');
+
+    const massage = {
+        loading : 'Загрузка',
+        success : 'Спасибо мы с Вами свяжемся',
+        failure : 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e)=> {
+            e.preventDefault();
+
+            const statusMassage = document.createElement('div');
+            statusMassage.classList.add('status');
+            statusMassage.textContent = massage.loading;
+            form.append(statusMassage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json');
+
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key) {
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMassage.textContent = massage.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMassage.remove();
+                    }, 2000);
+                } else {
+                    statusMassage.textContent = massage.failure;
+                }
+            });
+        });
+    }
 });
