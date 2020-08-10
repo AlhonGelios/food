33a50bbb876c1f5a6c4567250e238a53913fc42f
AlhonGelios <Geliosal39hon@gmail.com>
2020-08-10 14:43:24 +0300

    1123

--- src/js/main.js
+++ src/js/main.js
@@ -238,10 +238,22 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
-    function postData(form) {
+    const postData = async (url, data) => { 
+        const res = await fetch(url, {
+            method:'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body : data
+        });
+
+        return await res.json();
+    };
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e)=> {
             e.preventDefault();
 
@@ -260,13 +272,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 object[key] = value;
             });
 
-            fetch('server1.php', {
-                method:'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body : JSON.stringify(object)
-            }).then(data => data.text())
+            postData('http://localhost:3000/requests', JSON.stringify(object))
             .then((data) => {
                 console.log(data);
                 showThanksModal(message.success);
