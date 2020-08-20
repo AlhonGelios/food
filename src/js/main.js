a6c898b1be75291f3c0dc0839f7e2b66021e077a
Gelios <Geliosal39hon@gmail.com>
2020-08-20 21:59:56 +0300

    slider comp

--- src/js/main.js
+++ src/js/main.js
@@ -310,16 +310,27 @@ window.addEventListener('DOMContentLoaded', () => {
     let slideIndex = 1,
         offset = 0;
 
-    if (slides.length < 10) {
-        total.textContent = `0${slides.length}`;
-        current.textContent = `0${slideIndex}`;
-    } else if (slideIndex < 10) {
-        total.textContent = slides.length;
-        current.textContent = `0${slideIndex}`;
-    } else {
-        total.textContent = slides.length;
-        current.textContent = slideIndex;
+    function postIndexSlide () {
+        if (slides.length < 10) {
+            total.textContent = `0${slides.length}`;
+            current.textContent = `0${slideIndex}`;
+        } else if (slideIndex < 10) {
+            total.textContent = slides.length;
+            current.textContent = `0${slideIndex}`;
+        } else {
+            total.textContent = slides.length;
+            current.textContent = slideIndex;
+        }  
     }
+
+    function activeDots () {
+        dots.forEach(dot => {
+            dot.style.opacity = '0.5';
+            dots[slideIndex - 1].style.opacity = 1;
+        });
+    }
+
+    postIndexSlide();
 
     slidesField.style.width = 100 * slides.length + '%';
     slidesField.style.display = 'flex';
@@ -351,11 +362,15 @@ window.addEventListener('DOMContentLoaded', () => {
         dots.push(dot);
     }
 
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
+
     next.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == deleteNotDigits(width) * (slides.length - 1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -366,23 +381,15 @@ window.addEventListener('DOMContentLoaded', () => {
             slideIndex++;
         }
 
-        if (slideIndex < 10) {
-            current.textContent = `0${slideIndex}`;
-        } else {
-            current.textContent = slideIndex;
-        }
-
-        dots.forEach(dot => {
-            dot.style.opacity = '0.5';
-            dots[slideIndex - 1].style.opacity = 1;
-        });
+        postIndexSlide();
+        activeDots();
     });
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteNotDigits(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -393,16 +400,8 @@ window.addEventListener('DOMContentLoaded', () => {
             slideIndex--;
         }
 
-        if (slideIndex < 10) {
-            current.textContent = `0${slideIndex}`;
-        } else {
-            current.textContent = slideIndex;
-        }
-
-        dots.forEach(dot => {
-            dot.style.opacity = '0.5';
-            dots[slideIndex - 1].style.opacity = 1;
-        });
+        postIndexSlide();
+        activeDots();
     });
 
     dots.forEach(dot => {
@@ -410,20 +409,12 @@ window.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = deleteNotDigits(width) * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
-            if (slideIndex < 10) {
-                current.textContent = `0${slideIndex}`;
-            } else {
-                current.textContent = slideIndex;
-            }
-
-            dots.forEach(dot => {
-                dot.style.opacity = '0.5';
-                dots[slideIndex - 1].style.opacity = 1;
-            });
+            postIndexSlide();
+            activeDots();
         });
     });
 
